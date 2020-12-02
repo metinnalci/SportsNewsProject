92520d8c801a109e3ba13c21ab275d103baba846
Doğan Aker <m.doganaker@gmail.com>
2020-12-02 14:56:09 +0300

    Giriş sayfası ilk düzeni yapıldı

--- SportsNewsProject/Controllers/HomeController.cs
+++ SportsNewsProject/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SportsNewsProject.Models.ORM.Context;
+using SportsNewsProject.Models.VM;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,19 @@ namespace SportsNewsProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SportsNewsContext _newscontext;
+
+        public HomeController(SportsNewsContext newscontext)
+        {
+            _newscontext = newscontext;
+        }
         public IActionResult Index()
         {
-            return View();
+            HomeVM charts = new HomeVM();
+            charts.Categories = _newscontext.Categories.ToList();
+            charts.Authors = _newscontext.Authors.ToList();
+            charts.News = _newscontext.News.Include(q => q.AuthorCategory.Category).Include(q => q.AuthorCategory.Author).ToList();
+            return View(charts);
         }
     }
 }
---
+++ SportsNewsProject/Models/VM/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using SportsNewsProject.Models.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsNewsProject.Models.VM
+{
+    public class HomeVM
+    {
+        public List<News> News { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Author> Authors { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
