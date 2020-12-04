5796c7e644f45a6de02fe51313b6a96436c679da
metinnalci@outlook.com <metinnalci@outlook.com>
2020-12-04 15:37:45 +0300

    Postgresql entegrasyonu yapıldı.

--- SportsNewsProject/Startup.cs
+++ SportsNewsProject/Startup.cs
@@ -26,8 +26,9 @@ namespace SportsNewsProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<SportsNewsContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<SportsNewsContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
+            //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
