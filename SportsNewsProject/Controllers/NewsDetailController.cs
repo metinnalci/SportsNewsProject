eb4109ee5e418596087f76e79354ad08abdf1d41
metinnalci@outlook.com <metinnalci@outlook.com>
2020-12-23 15:14:09 +0300

    News Detail

---
+++ SportsNewsProject/Controllers/NewsDetailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using SportsNewsProject.Models.ORM.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsNewsProject.Controllers
+{
+    public class NewsDetailController : MainSiteController
+    {
+        private readonly SportsNewsContext _newscontext;
+
+        public NewsDetailController(SportsNewsContext newscontext, IMemoryCache memoryCache) : base(newscontext, memoryCache)
+        {
+            _newscontext = newscontext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
