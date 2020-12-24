dde0d441de051983a99e759aa4de04b43c96eddd
Doğan Aker <m.doganaker@gmail.com>
2020-12-24 19:46:28 +0300

    Issue #77 tamamlandı.

---
+++ SportsNewsProject/Controllers/CategoryPageController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using SportsNewsProject.Models.ORM.Context;
+using SportsNewsProject.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsNewsProject.Controllers
+{
+    public class CategoryPageController : MainSiteController
+    {
+        private readonly SportsNewsContext _newsContext;
+
+        public CategoryPageController(SportsNewsContext newsContext, IMemoryCache memoryCache) : base(newsContext, memoryCache)
+        {
+            _newsContext = newsContext;
+        }
+        public IActionResult Category(int id)
+        {
+            CategoryPageVM page = new CategoryPageVM();
+            page.category = _newsContext.Categories.Include(q => q.News.OrderByDescending(x => x.AddDate)).ThenInclude(News => News.Author).Where(q => q.IsDeleted == false).FirstOrDefault(x => x.ID == id);
+            return View(page);
+        }
+    }
+}
---
+++ SportsNewsProject/Models/VM/CategoryPageVM.cs
@@ -0,0 +1,14 @@
+﻿using SportsNewsProject.Models.ORM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsNewsProject.Models.VM
+{
+    public class CategoryPageVM
+    {
+        public Category category { get; set; }
+        public List<News> news { get; set; }
+    }
+}
