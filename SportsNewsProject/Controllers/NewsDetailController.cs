0a594b3460e755fb12c40ac727c4da0ff8923e4a
metinnalci@outlook.com <metinnalci@outlook.com>
2020-12-25 18:46:35 +0300

    News Detail sayfasına yorum ekleme.

--- SportsNewsProject/Controllers/NewsDetailController.cs
+++ SportsNewsProject/Controllers/NewsDetailController.cs
@@ -20,36 +20,43 @@ namespace SportsNewsProject.Controllers
             _newscontext = newscontext;
         }
 
-        public IActionResult Index(int id)
+        public IActionResult Index(int id, NewsVM? model)
         {
-            NewsVM model = new NewsVM();
+            NewsVM modell = new NewsVM();
             News news = new News();
+            id = model.ID;
             news = _newscontext.News.Include(q => q.Author).Include(q => q.CommentList).ThenInclude(q => q.User).FirstOrDefault(x => x.ID == id);
 
-            model.Title = news.Title;
-            model.SubTitle = news.SubTitle;
-            model.AuthorName = news.Author.Name;
-            model.Content = news.Content;
-            model.AddDate = news.AddDate;
-            model.Comments = news.CommentList.Where(q => q.IsDeleted == false).OrderByDescending(q => q.AddDate).ToList();
+            modell.ID = id;
+            modell.Title = news.Title;
+            modell.SubTitle = news.SubTitle;
+            modell.AuthorName = news.Author.Name;
+            modell.Content = news.Content;
+            modell.AddDate = news.AddDate;
+            modell.Comments = news.CommentList.Where(q => q.IsDeleted == false).OrderByDescending(q => q.AddDate).ToList();
             
-            return View(model);
+            return View(modell);
         }
+
 
         [HttpPost]
         public IActionResult AddComment(NewsVM model)
         {
             Comment comment = new Comment();
-            comment.Content = model.Comment.Content;
+            User user = new User();
+
+            //int id = model.ID;
             comment.NewsId = model.ID;
-            comment.UserId = model.Comment.User.ID;
-            comment.User.EMail = model.Comment.User.EMail;
-            comment.User.NickName = model.Comment.User.NickName;
+            comment.Content = model.Comment.Content;
+            user = _newscontext.Users.Where(q => q.EMail == model.Comment.User.EMail).FirstOrDefault();
+            comment.UserId = user.ID;
+            //comment.User.EMail = user.EMail;
+            //comment.User.NickName = user.NickName;
 
             _newscontext.Comments.Add(comment);
             _newscontext.SaveChanges();
             
-            return RedirectToAction("NewsDetail","Index");
+            return RedirectToAction("Index", "NewsDetail", model);
         }
     }
 }
