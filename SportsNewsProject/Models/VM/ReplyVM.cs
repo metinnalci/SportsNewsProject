bed5100b664ac9dbae74d6c69cbff260afe649d6
metinnalci@outlook.com <metinnalci@outlook.com>
2021-01-02 19:00:16 +0300

    Comment Reply işlemi

---
+++ SportsNewsProject/Models/VM/ReplyVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsNewsProject.Models.VM
+{
+    public class ReplyVM
+    {
+        public int parentid { get; set; }
+        public string useremail { get; set; }
+        public int newsid { get; set; }
+        public string replycomment { get; set; }
+
+    }
+}
